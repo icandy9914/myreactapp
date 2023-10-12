3964c9fbe9a1d08a1f101d5351e30e46abd52e9d
Ganesh Vishwakarma <it.ganeshvishwakarma@gmail.com>
2023-10-12 19:04:22 +0530

    Enhanded react code

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,9 +1,15 @@
 import React,{useState} from 'react'
 
 export default function TextForms(props) {
-    const handleClick = ()=> { 
+    const handleUpperClick = ()=> { 
         console.log("you clicked" + mytxt);
         let newTxt = mytxt.toUpperCase();
+        setText(newTxt);
+    }
+
+    const handleLowerClick = ()=> { 
+        console.log("you clicked" + mytxt);
+        let newTxt = mytxt.toLowerCase();
         setText(newTxt);
     }
     const handleUpdate = (event)=> {
@@ -16,13 +22,17 @@ export default function TextForms(props) {
         <div className="container">
             <h1>{props.headingTxt} - </h1>
             <div className="mb-3">
-                <textarea value={mytxt} className="form-control" id="exampleFormControlTextarea1" rows="10" onChange={handleUpdate}>{mytxt}</textarea>
-                <button className="btn btn-primary" onClick={handleClick}>Process now</button>
+                <textarea value={mytxt} className="form-control my-3" id="exampleFormControlTextarea1" rows="10" onChange={handleUpdate}>{mytxt}</textarea>
+                <button className="btn btn-primary mx-2" onClick={handleUpperClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary" onClick={handleLowerClick}>Convert to Lowercase</button>
             </div>
         </div>
         <div className="container">
             <h2>Your Text Summary</h2>
             <p>{mytxt.split(" ").length} words & {mytxt.length} characters</p>
+            <p>{0.008 * mytxt.split(" ").length} Minutes took to read</p>
+            <h3>Preview Text</h3>
+            <p>{mytxt}</p>
         </div>
         </>
     )
