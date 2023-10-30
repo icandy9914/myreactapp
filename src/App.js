f2608f28604d57a2f09381beea19682f6d633d32
Ganesh Vishwakarma <it.ganeshvishwakarma@gmail.com>
2023-10-31 00:14:29 +0530

    darkmode featured implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForms from './components/TextForms';
-// import About from './components/About';
+import About from './components/About';
+import { useState } from 'react';
+
 
 function App() {
+  let [mode,setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor='#212529';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor='#fff';
+    }
+  }
   return (
     <>
     
-<Navbar title="Hi Ganesh" aboutTxt="Vishwakrama"/>
-<TextForms headingTxt="Write your notes here"/>
-{/* <Navbar/> */}
+<Navbar title="Hi Ganesh" aboutTxt="Vishwakrama" mode={mode} toggleMode={toggleMode}/>
 {/* <About/> */}
+<TextForms headingTxt="Write your notes here" mode={mode}/>
+
 </>
   );
 }
